3415a88ab4b12fcd8be3c6cdb0208ea7dbc63d5c
Matty <matyshemi@gmail.com>
2025-06-30 20:03:41 -0400

    updated

--- src/API/axiosConfig.js
+++ src/API/axiosConfig.js
@@ -3,4 +3,11 @@ import axios from "axios";
 const axiosBase = axios.create({
   baseURL: "https://evangadi-q-a-backend.onrender.com/api",
 });
+// axiosBase.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
 export default axiosBase;
